0382b26089f5b23f02813175182a66b18d64e488
Mba96 <mbajameel90@gmail.com>
2023-07-24 09:02:39 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModal = document.querySelector(".close-modal");
+const openModal = document.querySelectorAll(".show-modal")
