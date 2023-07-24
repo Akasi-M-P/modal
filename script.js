383136283194809a4b492e6e0ccae7f97efe2364
Peter M. Akasi <118350559+Akasi-M-P@users.noreply.github.com>
2023-07-24 09:43:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// Selecting the modal, overlay, close button, and all elements that trigger modal opening
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const closeModal = document.querySelector(".close-modal");
-const openModal = document.querySelectorAll(".show-modal")
+const openModal = document.querySelectorAll('.show-modal');
+
+// Function to open the modal and display the overlay
+const openModalWindow = () => {
+  modal.classList.remove('hidden');   // Remove the 'hidden' class to show the modal
+  overlay.classList.remove('hidden'); // Remove the 'hidden' class to show the overlay
+};
+
+// Function to close the modal and hide the overlay
+const closeModalWindow = () => {
+  modal.classList.add('hidden');      // Add the 'hidden' class to hide the modal
+  overlay.classList.add('hidden');    // Add the 'hidden' class to hide the overlay
+};
+
+// Adding click event listeners to all elements that trigger modal opening
+for (let i = 0; i < openModal.length; i++) {
+  openModal[i].addEventListener('click', openModalWindow);
+}
+
+// Adding click event listener to the close button to close the modal
+closeModal.addEventListener('click', closeModalWindow);
+
+// Adding click event listener to the overlay to close the modal if clicked outside the modal
+overlay.addEventListener('click', closeModalWindow);
